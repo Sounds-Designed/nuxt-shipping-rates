7e1c7d8e0ac19e3aaea9085ccebdca77fe6ea1ae
Alex Scott <codetheorist@outlook.com>
2026-01-09 01:03:32 +0000

    feat: demo data in playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,11 @@
+import { defineNuxtConfig } from "nuxt/config";
+import ShippingRates from "./shipping-rates.json"
+import type { ShippingRate } from "../src/module";
+
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
-  inkthreadable: {},
+  shippingRates: {
+    rates: ShippingRates as ShippingRate[]
+  }
 })
